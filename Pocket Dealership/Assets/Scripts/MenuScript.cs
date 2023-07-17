1e7544e621fec6f2be732fd07754ad8a175a83c1
JediHare <nathan20071010@gmail.com>
2023-07-18 01:47:36 +0200

    Settings Menu + Safe Areas
    In elke scene zit een safe area nu en er is een settings menu gemaakt.

---
+++ Pocket Dealership/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void OpenSettingsMenu()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void BackToGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
